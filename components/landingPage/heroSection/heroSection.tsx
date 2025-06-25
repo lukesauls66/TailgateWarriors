d65bb708e34299ef95c79af0c02c1f3263af7c68
William Lyman <luke.sauls279@gmail.com>
2025-06-24 17:32:35 -0700

    hero section init

---
+++ components/landingPage/heroSection/heroSection.tsx
@@ -0,0 +1,14 @@
+export default function HeroSection() {
+  return (
+    <section>
+      <img src="/tw-hero-image.PNG" alt="Hero Image" className="w-100%" />
+      <div>
+        <h2>Gear Up for the Ultimate Tailgating Experience</h2>
+        <p>
+          Discover the best equipment, techniques, and strategies to take your
+          performance to the next level.
+        </p>
+      </div>
+    </section>
+  );
+}
