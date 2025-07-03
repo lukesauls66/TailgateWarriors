b9da54e1747e334053180eba1bfb73f357c30a6a
William Lyman <luke.sauls279@gmail.com>
2025-07-03 13:52:11 -0700

    made submitButton reusable

--- components/FAQPage/FAQPage.tsx
+++ components/FAQPage/FAQPage.tsx
@@ -1,3 +1,10 @@
+import ContactSection from "../utils/contactSection";
+
 export default function FAQPage() {
-  return <h1>FAQs</h1>;
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1>FAQs</h1>
+      <ContactSection />
+    </main>
+  );
 }
