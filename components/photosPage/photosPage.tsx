89ecc264b01e594fe7c8c9866021443c2058b2d4
Luke Sauls <luke.sauls279@gmail.com>
2025-09-02 17:25:58 -0700

    init photos page to pull photos from db

--- components/photosPage/photosPage.tsx
+++ components/photosPage/photosPage.tsx
@@ -1,3 +1,21 @@
-export default function PhotosPage() {
-  return <h1>Photos</h1>;
+"use client";
+
+export type Photo = {
+  id: string;
+  url: string;
+  category: string;
+};
+
+interface PhotosPageProps {
+  photos: Photo[];
+}
+
+export default function PhotosPage({ photos }: PhotosPageProps) {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-4 md:p-8 lg:p-12 gap-4 md:gap-8 lg:gap-12">
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-center">
+        Photos
+      </h1>
+    </main>
+  );
 }
